5b4e55b648869a8f037299c048a8adf6d7b1baa5
Shmuel Zeigerman <solomuz0@gmail.com>
2012-06-28 20:31:28 +0300

    Add files.

---
+++ unicode_far/FarGuid.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+/*
+FarGuid.hpp
+
+GUID'ы фара.
+*/
+/*
+Copyright © 2010 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// {00000000-0000-0000-0000-000000000000}
+DEFINE_GUID(FarGuid,0,0,0,0,0,0,0,0,0,0,0);
+// {773B5051-7C5F-4920-A201-68051C4176A4}
+DEFINE_GUID(NetworkGuid, 0x773b5051, 0x7c5f, 0x4920, 0xa2, 0x1, 0x68, 0x5, 0x1c, 0x41, 0x76, 0xa4);
+// {742910F1-02ED-4542-851F-DEE37C2E13B2}
+DEFINE_GUID(EMenuGuid, 0x742910f1, 0x2ed, 0x4542, 0x85, 0x1f, 0xde, 0xe3, 0x7c, 0x2e, 0x13, 0xb2);
+// {65642111-AA69-4B84-B4B8-9249579EC4FA}
+DEFINE_GUID(ArcliteGuid, 0x65642111, 0xaa69, 0x4b84, 0xb4, 0xb8, 0x92, 0x49, 0x57, 0x9e, 0xc4, 0xfa);
+// {4EBBEFC8-2084-4B7F-94C0-692CE136894D}
+DEFINE_GUID(LuamacroGuid, 0x4ebbefc8, 0x2084, 0x4b7f, 0x94, 0xc0, 0x69, 0x2c, 0xe1, 0x36, 0x89, 0x4d);
---
+++ unicode_far/vbuild.m4
@@ -0,0 +1,1 @@
+m4_define(BUILD,2753)m4_dnl
